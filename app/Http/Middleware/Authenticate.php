f216dc93e7803e4e7d8d8370d7ddd9cb6deb33e2
romashorodok <108732029+romashorodok@users.noreply.github.com>
2022-07-07 20:38:55 +0300

    feat: middleware for recognizing unauthorized users

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,16 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return Auth::check()
+            ? $next($request)
+            : response(['message' => "Unauthorized"], 401);
     }
 }
