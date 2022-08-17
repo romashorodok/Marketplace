95e4d9c6c14e47113535aab9d692b2534889ca56
romashorodok <108732029+romashorodok@users.noreply.github.com>
2022-08-17 22:21:43 +0300

    feat: added order migration

---
+++ database/migrations/2022_08_17_104814_create_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->decimal('total_price', 10, 2);
+            $table->string('charge_token')->nullable();
+
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('address');
+
+            $table->foreignId('user_id')
+                ->constrained('users');
+        });
+
+        Schema::table('billing_items', function (Blueprint $table) {
+            $table->foreignId('order_id')
+                ->nullable()
+                ->constrained('billing_items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+        Schema::table('billing_items', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('order_id');
+        });
+    }
+};
