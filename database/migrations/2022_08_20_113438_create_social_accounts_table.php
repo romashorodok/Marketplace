fe5ed66b81f64a7f8e7679021dac0af2474defb4
romashorodok <108732029+romashorodok@users.noreply.github.com>
2022-08-25 20:54:58 +0300

    feat: added migration.

---
+++ database/migrations/2022_08_20_113438_create_social_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('social_id')->index();
+            $table->string('social_provider')->index();
+            $table->string('social_token');
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+};
