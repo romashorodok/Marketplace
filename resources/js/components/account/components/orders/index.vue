74cdbed4451b8aceeb1b0e45bfe8eaa26830867a
romashorodok <108732029+romashorodok@users.noreply.github.com>
2022-08-30 21:05:43 +0300

    feat: added empty orders icons.

--- resources/js/components/account/components/orders/index.vue
+++ resources/js/components/account/components/orders/index.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="order-wrapper">
         <div class="order-list">
-            <ul class="list-wrapper">
+            <ul v-if="orders.length !== 0" class="list-wrapper">
                 <app-order v-for="order in orders" :order="order"/>
             </ul>
+            <div v-else class="empty-section">
+                <img src="/icons/orders-outline.svg" width="40" alt="test"/>
+                <p>You don't have any orders</p>
+            </div>
         </div>
         <app-pagination :current-page="page" :pages="pages" @onChange="changePage"/>
     </div>
